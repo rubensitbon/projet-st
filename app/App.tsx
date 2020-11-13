99bae0069f9d10bc719dd9f640cf0531f4a92a61
Ruben Sitbon <ruben.sitbon@outlook.com>
2020-11-13 13:58:29 +0100

    move screens to src

--- app/App.tsx
+++ app/App.tsx
@@ -1,8 +1,8 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import * as firebase from 'firebase';
 import { firebaseConfig } from './config/firebase';
-import AuthNavigator from './screens/AuthNavigator';
-import HomeScreen from './screens/HomeScreen';
+import AuthNavigator from 'screens/AuthNavigator';
+import HomeScreen from 'screens/HomeScreen';
 
 firebase.initializeApp(firebaseConfig);
 
